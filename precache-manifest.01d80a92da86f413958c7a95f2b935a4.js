6c5b53c5b1f2ec9a0a8f9a4dc8108ca8d5ce8fac
Big-Apisit <apisit.junyavarangkoon@hotmail.com>
2023-01-30 21:57:31 +0700

    Updates

---
+++ precache-manifest.01d80a92da86f413958c7a95f2b935a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "43c0c005ebf7cf44581b",
+    "url": "/react-big-shop/static/css/main.ca4d1462.chunk.css"
+  },
+  {
+    "revision": "43c0c005ebf7cf44581b",
+    "url": "/react-big-shop/static/js/main.43c0c005.chunk.js"
+  },
+  {
+    "revision": "62e66383286da32eaa99",
+    "url": "/react-big-shop/static/css/1.32876bad.chunk.css"
+  },
+  {
+    "revision": "62e66383286da32eaa99",
+    "url": "/react-big-shop/static/js/1.62e66383.chunk.js"
+  },
+  {
+    "revision": "39af5cbdf979425647c4",
+    "url": "/react-big-shop/static/js/runtime~main.39af5cbd.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/react-big-shop/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "9abedde93bb5b7c32d8335c02aa003d7",
+    "url": "/react-big-shop/index.html"
+  }
+];
